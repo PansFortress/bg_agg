c85d26c9582cf7ca8dbe0cce400d554b19de7ee3
PansFortress <Japan8789@gmail.com>
2017-02-09 15:54:30 -0500

    Revised review form to display gamename and fixed alignment issue

--- bg_agg/views.py
+++ bg_agg/views.py
@@ -1,10 +1,15 @@
 from flask import request, redirect, url_for, render_template, flash
-from . import app
+from . import app, models
+from .database import Base, engine, session
 
 @app.route("/")
 def default():
     return render_template("product.html")
 
-@app.route("/form")
-def form():
-    return render_template("review_form.html")
+@app.route("/game/<int:game_id>/form")
+def form(game_id):
+    Product = models.Product
+    product = session.query(Product).filter(Product.id==game_id).first()
+    if product:
+        return render_template("review_form.html", product=product)
+    return render_template("404.html"),404
