ab7e77cc2f9daedf36e4e0149ac4614e8435579d
= <Japan8789@gmail.com>
2017-02-14 21:12:23 -0500

    Removing config

--- bg_agg/config.py
+++ bg_agg/config.py
@@ -8,3 +8,7 @@ class DevelopmentConfig(object):
 class TestingConfig(object):
     DATABASE_URI = "postgresql:///bg_agg_test"
     DEBUG = True
+
+class HerokuDevConfig(object):
+    DATABASE_URI = "postgres://unszlllddfwith:cfd28d506d106f8f4986e75fa6ade194defe8cf17dd307e367a0fc7f739e60d9@ec2-174-129-37-15.compute-1.amazonaws.com:5432/dfuusin4kespkh"
+    SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(12))
